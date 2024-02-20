d521ddd851b72bb2c6ef870cc35613eeca300569
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-20 08:13:22 +0800

    exclude files that start with "."

--- rotate_images.py
+++ rotate_images.py
@@ -11,16 +11,17 @@ args = vars(ap.parse_args())
 if os.path.isfile(args["images"]):
     images = [args["images"].split("/")[-1]]
 else:
-    images = [f for f in os.listdir(args["images"]) if os.path.isfile(os.path.join(args["images"], f))]
+    images = [f for f in os.listdir(args["images"]) if os.path.isfile(os.path.join(args["images"], f)) and f[0] != "."]
 
 for image in images:
     image_name = image.split(".")[0]
     image_extension = image.split(".")[1]
     image_full_path = os.path.join(args["images"], image)
+    print(f"[INFO] Processing {image_full_path}")
     image_object = cv2.imread(image_full_path)
     for angle in range(90, 271, 90):
         rotated = imutils.rotate_bound(image_object, angle) # looks like rotate bound will shrink the image a bit
         output_image_name = image_name + "_" + str(angle) + "." + image_extension
         output_image_path = os.path.join(args["output"], output_image_name)
-        print(output_image_path)
         cv2.imwrite(output_image_path, rotated)
+        print(f"[INFO] Generated {output_image_path}")
